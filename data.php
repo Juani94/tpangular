51c52c3997d1fa3210b3cfd717f587ec5279b592
Juani94 <juanitolanzara94@gmail.com>
2017-07-01 18:46:30 -0300

    Add files via upload

---
+++ data.php
@@ -0,0 +1,26 @@
+<?php header("Access-Control-Allow-Origin: *")
+$data = array(
+array( "foto"=>"MAXIMUS" ,
+		"titulo"=>"Maximus Festival" ,
+	    "banda"=>"varios",
+	   "tipo"=>"Festival",
+	  "lugar"=>"Tecnopolis",
+	  "fecha"=>"20/05/2018",
+	  "descripcion"=>("Festival de bandas de metal en argentina" )),
+array(
+	"foto"=>"tarja-turunen" ,
+    "titulo"=>"Tarja en argentina",
+    "banda"=>"Tarja Turunen",
+    "tipo"=>"Concierto",
+    "lugar"=>"Luna Park",
+    "fecha"=>"25/11/2017",
+    "descripcion"=>"concierto de la gira de Tarja Turunen en buenos aires de su gira The Shadow Shows")),
+
+);
+
+
+
+
+json_encode($data);
+
+?>
